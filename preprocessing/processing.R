c8c342d2aebfc8bab4d30ef4b403da820588977b
Roy-Ghandour <ghandourroy@gmail.com>
2025-05-05 20:00:18 +0100

    Updated preprocessing folder

--- preprocessing/processing.R
+++ preprocessing/processing.R
@@ -1,22 +1,16 @@
-
 # Imports
+library(readr)
 library(dplyr)
+library(lubridate)
 
 # Read in the CSV file
 reports <- read.csv("mc1-reports-data.csv", stringsAsFactors = FALSE)
 
-# Aggregate by neighborhood (location)
-agg_data <- reports %>%
-  group_by(location) %>%
-  summarise(
-    avg_sewer_and_water = mean(sewer_and_water, na.rm = TRUE),
-    avg_power = mean(power, na.rm = TRUE),
-    avg_roads_and_bridges = mean(roads_and_bridges, na.rm = TRUE),
-    avg_medical = mean(medical, na.rm = TRUE),
-    avg_buildings = mean(buildings, na.rm = TRUE),
-    avg_shake_intensity = mean(shake_intensity, na.rm = TRUE),
-    .groups = "drop"
-  )
+# Filter out invalid times
+reports <- reports %>%
+  mutate(time_parsed = ymd_hms(time)) %>%
+  filter(minute(time_parsed) %% 5 == 0 & second(time_parsed) == 0) %>%
+  select(-time_parsed)
 
 # Save aggregated data to CSV
-write.csv(agg_data, "aggregated_reports.csv", row.names = FALSE)
+write.csv(reports, "reports.csv", row.names = FALSE)
